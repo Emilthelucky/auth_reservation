a86632b1cddc3d1c5ed8b6d3c6dd5d42e7102be9
GitHub Kullanıcı Adınız <asifadisirinov@gmail.com>
2024-09-06 13:51:45 +0400

    update

--- dist/utils/EmailValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ValidateEamil = void 0;
-const ValidateEamil = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-};
-exports.ValidateEamil = ValidateEamil;
-//# sourceMappingURL=EmailValidation.js.map
--- dist/utils/StrongPassword.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ValidateStrongPassword = void 0;
-function ValidateStrongPassword(password) {
-    const strongPasswordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    return strongPasswordRegex.test(password);
-}
-exports.ValidateStrongPassword = ValidateStrongPassword;
-//# sourceMappingURL=StrongPassword.js.map
