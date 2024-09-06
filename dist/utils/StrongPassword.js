d1417ad62d6235bbf845b79eae8a99780994dbda
GitHub Kullanıcı Adınız <asifadisirinov@gmail.com>
2024-09-06 14:27:09 +0400

    fix

--- dist/utils/StrongPassword.js
+++ dist/utils/StrongPassword.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ValidateStrongPassword = void 0;
+exports.ValidateStrongPassword = ValidateStrongPassword;
 function ValidateStrongPassword(password) {
     var strongPasswordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     return strongPasswordRegex.test(password);
 }
-exports.ValidateStrongPassword = ValidateStrongPassword;
 //# sourceMappingURL=StrongPassword.js.map
